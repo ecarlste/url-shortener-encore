5e6d8929277532fc32f6058277e0ce6431b82bc6
Erik S. Carlsten <ecarlste@gmail.com>
2025-02-18 14:08:51 -0700

    save urls in a database

--- url/url.ts
+++ url/url.ts
@@ -1,5 +1,8 @@
 import { api } from "encore.dev/api";
+import { SQLDatabase } from "encore.dev/storage/sqldb";
 import { randomBytes } from "node:crypto";
+
+const db = new SQLDatabase("url", { migrations: "./migrations" });
 
 interface URL {
   id: string;
@@ -14,6 +17,12 @@ export const shorten = api(
   { method: "POST", path: "/url", expose: true },
   async ({ url }: ShortenParams): Promise<URL> => {
     const id = randomBytes(6).toString("base64url");
+
+    await db.exec`
+      INSERT INTO url (id, original_url)
+      VALUES (${id}, ${url})
+    `;
+
     return { id, url };
   }
 );
